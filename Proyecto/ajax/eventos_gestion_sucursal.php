e6ce62a37bff9fbe2732521b189fb11a22a002cf
QhorinElManco <43563235+QhorinElManco@users.noreply.github.com>
2019-04-19 16:32:10 -0600

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/QhorinElManco/Proyecto-Bases-I""""
    This reverts commit 3072e0ebcc94d830b29a7c5f9aeb6e11a00d1943.

--- Proyecto/ajax/eventos_gestion_sucursal.php
+++ Proyecto/ajax/eventos_gestion_sucursal.php
@@ -3,134 +3,25 @@
     switch($_GET["accion"]){
         case "1":
             $conexion = new Conexion();
-            $sql = "SELECT idSucursal, nombre, direccion FROM sucursal";
+            $sql = "SELECT * FROM sucursal";
             $resultado = $conexion->ejecutarInstruccion($sql);	
             if(!$resultado){
-                echo "alert(`Hay una error en la sentencia sql:`.$sql)";
+                echo "Hay una error en la sentencia sql: ".$sql;
             }
             else{
                 while($fila=$conexion->obtenerFila($resultado)){
-                    
-                       echo '<tr>
-                                <th scope="row">'.$fila["idSucursal"].'</th>
-                                <td>'.$fila["nombre"].'</td>
-                                <td>'.$fila["direccion"].'</td>
-                                <td><button class="btn btn-warning btn btn-success" type="button" data-toggle="modal"
-                                data-target="#Modal2"><i class="mdi mdi-tooltip-edit"></i></button></td>
-                                <td><button class="btn btn-danger eliminar" type="button" data-toggle="modal"
-                                data-target="#Modal3"><i class="mdi mdi-playlist-remove"></i></button></td>
-                            </tr>';}
+                    $lista[]=$fila;
+                }
+                echo json_encode($lista);
+
             }
-            $conexion->liberarResultado($resultado);
             $conexion->cerrarConexion();
         break;
-
-        case "2":
-            if(isset($_POST["nombre"])){
-                $nombre = $_POST["nombre"];
-            }
-            if(isset($_POST["direccion"])){
-                $direccion = $_POST["direccion"];
-            }
-            if($nombre=="" || $nombre==NULL){
-                $respuesta="Ingrese el nombre";
-                echo $respuesta;
-            }
-            else if($direccion=="" || $direccion==NULL){
-                $respuesta="Ingrese la direccion";
-                echo $respuesta;
-            }
-            else{
-                $conexion = new Conexion();
-                $accion = "AGREGAR";
-                $sql = "CALL SP_GESTION_SUCURSAL('0', '$nombre','$direccion','$accion', @p4, @p5);";
-                $salida = "SELECT @p4 AS mensaje, @p5 AS codigo;";
-                $resultado = $conexion->ejecutarInstruccion($sql);
-                $respuesta = $conexion->ejecutarInstruccion($salida);
-                if(!$respuesta){
-                    echo "No hay respuesta del procedimiento";
-                }
-                else{
-                    $fila=$conexion->obtenerFila($respuesta);
-                    echo $fila["mensaje"];
-                }
-                $conexion->cerrarConexion();
-            }
-
-        break;
-
-        case "3":
-        if(isset($_POST["id"])){
-            $id = $_POST["id"];
-        }
-        if(isset($_POST["nombre"])){
-            $nombre = $_POST["nombre"];
-        }
-        if(isset($_POST["direccion"])){
-            $direccion = $_POST["direccion"];
-        }
-        if($id=="" || $id==NULL){
-            $respuesta="Ingrese el id";
-            echo $respuesta;
-        }
-        if($nombre=="" || $nombre==NULL){
-            $respuesta="Ingrese el nombre";
-            echo $respuesta;
-        }
-        else if($direccion=="" || $direccion==NULL){
-            $respuesta="Ingrese la direccion";
-            echo $respuesta;
-        }
-        else{
-            $conexion = new Conexion();
-            $accion = "EDITAR";
-            $sql = "CALL SP_GESTION_SUCURSAL('$id', '$nombre','$direccion','$accion', @p4, @p5);";
-            $salida = "SELECT @p4 AS mensaje, @p5 AS codigo;";
-            $resultado = $conexion->ejecutarInstruccion($sql);
-            $respuesta = $conexion->ejecutarInstruccion($salida);
-            if(!$respuesta){
-                echo "No hay respuesta del procedimiento";
-            }
-            else{
-                $fila=$conexion->obtenerFila($respuesta);
-                echo $fila["mensaje"];
-            }
-            $conexion->cerrarConexion();
-        }
-        break;
-
-        case "4":
-            if(isset($_POST["id"])){
-                $id=$_POST["id"];
-            }
-            if($id=="" || $id==NULL){
-                $respuesta="Ingrese el id";
-                echo $respuesta;
-            }
-            else{
-                $conexion= new conexion();
-                $accion="ELIMINAR";
-                $sql="CALL SP_GESTION_SUCURSAL('$id', @p1, @p2, '$accion', @p4, @p5);";
-                $salida = "SELECT @p4 AS mensaje, @p5 AS codigo";
-                $resultado = $conexion->ejecutarInstruccion($sql);
-                $respuesta = $conexion->ejecutarInstruccion($salida);
-                if(!$respuesta){
-                    echo "No hay respuesta del procedimiento";
-                }
-                else{
-                    $fila=$conexion->obtenerFila($respuesta);
-                    echo $fila["mensaje"];
-                }
-                $conexion->cerrarConexion();
-
-            }
-        break;
-    
+        
         default:
-            echo "No registro la opcion";
-        break;
-    };
+            echo json_encode("No registro la opcion");
+            break;
+    }
+    $conexion->liberarResultado($resultado);
 
-
-
-?>;
+?>
